6497feffb4f30aab29322d5923308b0e94b4b42c
FarrapoLucas <lukaswhelp@gmail.com>
2024-04-11 10:41:56 -0300

    aula js condicionais

---
+++ condicionais/js/exercicio2.js
@@ -0,0 +1,8 @@
+var país = 'EUA'
+console.log(`Viveno no ${país}`)
+    if (país == 'Brasil'){
+        console.log{'Você é brasileiro'}
+    }
+    else{
+        console.log{'Você é estrangeiro'}
+    }
